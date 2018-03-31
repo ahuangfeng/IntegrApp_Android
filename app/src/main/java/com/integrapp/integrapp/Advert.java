1d8f20c7dd54159a18778191915d8f0c77c01e71
Roger López <rogerlopezbelmonte@gmail.com>
2018-03-31 22:26:59 +0200

    Changed menu options and icons

--- app/src/main/java/com/integrapp/integrapp/Advert.java
+++ app/src/main/java/com/integrapp/integrapp/Advert.java
@@ -80,17 +80,19 @@ public class Advert extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_adverts) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_forum) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_chats) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_aboutUs) {
+
+        } else if (id == R.id.nav_logOut) {
 
         }
 
