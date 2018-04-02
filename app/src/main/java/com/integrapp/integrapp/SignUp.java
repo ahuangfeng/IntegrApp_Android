f054e20a5de884c218c62b1787f3ace91dfbca82
Daniel Perez Fernandez <danielpf42@gmail.com>
2018-04-02 22:37:55 +0200

    Connection of SignUp with server works. TO DO: error control

--- app/src/main/java/com/integrapp/integrapp/SignUp.java
+++ app/src/main/java/com/integrapp/integrapp/SignUp.java
@@ -1,16 +1,27 @@
 package com.integrapp.integrapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Objects;
 
 public class SignUp extends AppCompatActivity {
@@ -20,6 +31,12 @@ public class SignUp extends AppCompatActivity {
     LinearLayout layoutCIF;
     View viewCIF;
     Button signUpButton;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    EditText cifEditText;
+    private String itemSelectedSpinner;
+
+    private String serverResponse = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +52,7 @@ public class SignUp extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String elem = parent.getItemAtPosition(position).toString();
+                passFromSpinner(elem); //pass from spinner to server format
                 layoutCIF = (LinearLayout) findViewById(R.id.layoutCIF);
                 viewCIF = (View) findViewById(R.id.viewCIF);
 
@@ -59,13 +77,93 @@ public class SignUp extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Create successful", Toast.LENGTH_SHORT).show();
 
-                Intent i = new Intent(SignUp.this, Advert.class);
-                startActivity(i);
-                finish();
-                LogIn.logInActivity.finish();
+                usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+                passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+                cifEditText = (EditText) findViewById(R.id.cifEditText);
+
+                String user = usernameEditText.getText().toString();
+                String pass = passwordEditText.getText().toString();
+                String cif = cifEditText.getText().toString();
+
+                if (((user.isEmpty() || pass.isEmpty()) && !itemSelectedSpinner.equals("association"))
+                        || ((user.isEmpty() || pass.isEmpty() || cif.isEmpty()) && itemSelectedSpinner.equals("association"))) {
+                    Toast.makeText(getApplicationContext(), "Fill in all the fields, please.", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    sendDataToServer(user, pass, cif);
+                    if (!serverResponse.equals("ERROR IN SIGNUP")) {
+                        Toast.makeText(getApplicationContext(), "Create successful", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(SignUp.this, Advert.class);
+                        startActivity(i);
+                        finish();
+                        LogIn.logInActivity.finish();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "User already exists", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
+    }
+
+    private void passFromSpinner(String element) {
+        if (element.equals("Association")) itemSelectedSpinner = "association";
+        else if (element.equals("Voluntary")) itemSelectedSpinner = "voluntary";
+        else if (element.equals("NewComer")) itemSelectedSpinner = "newComer";
+        else itemSelectedSpinner = "admin";
+    }
+
+    private String generateRequestRegister(String username, String password, String cif) throws JSONException {
+
+        JSONObject oJSON = new JSONObject();
+
+        oJSON.put("username", username);
+        oJSON.put("password", password);
+        oJSON.put("type", itemSelectedSpinner);
+
+        if (itemSelectedSpinner.equals("association")) {
+            oJSON.put("CIF", cif);
+        }
+        return oJSON.toString(1);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private void sendDataToServer(String username, String password, String cif) {
+
+        try {
+            final String json = generateRequestRegister(username, password, cif);
+            new AsyncTask<Void, Void, String>() {
+                @Override
+                protected String doInBackground(Void... voids) {
+                    return getServerResponse(json);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    System.out.println("SERVER RESPONSE: " + s);
+                }
+            }.execute();
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getServerResponse(String json) {
+        HttpPost post = new HttpPost("https://integrappbackend.herokuapp.com/api/register");
+        try {
+            StringEntity entity = new StringEntity(json);
+            post.setEntity(entity);
+            post.setHeader("Content-type", "application/json");
+
+            DefaultHttpClient client = new DefaultHttpClient();
+            BasicResponseHandler handler = new BasicResponseHandler();
+            return client.execute(post, handler);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "ERROR IN SIGNUP";
     }
 }
