1542e17fbe5c78104a47901c9ced2cfe22686238
Roger Lopez <roger.lopez.belmonte@est.fib.upc.edu>
2018-04-17 13:52:01 +0200

    New Advert development#2

---
+++ app/src/main/java/com/integrapp/integrapp/NewAdvertFragment.java
@@ -0,0 +1,29 @@
+package com.integrapp.integrapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NewAdvertFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.activity_new_advert, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+
+}
