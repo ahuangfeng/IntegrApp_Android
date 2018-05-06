bc3079e15f26f1088fbbcfc78ef5b88296585305
Manel Alexandre <garcia.martin.manelalex@gmail.com>
2018-05-06 16:47:57 +0200

    reduce ads preferences when delete advert

--- app/src/main/java/com/integrapp/integrapp/SingleAdvertFragment.java
+++ app/src/main/java/com/integrapp/integrapp/SingleAdvertFragment.java
@@ -376,6 +376,12 @@ public class SingleAdvertFragment extends Fragment {
                 if (!s.equals("ERROR IN DELETING ADVERT")) {
                     System.out.println("DELETE ADVERT SUCCESSFULL RESPONSE: " +s);
                     Toast.makeText(getActivity().getApplicationContext(), "Advert deleted successfully", Toast.LENGTH_SHORT).show();
+                    SharedPreferences preferences = getActivity().getSharedPreferences("login_data", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    int ads = preferences.getInt("ads", 0);
+                    editor.putInt("ads", ads+1);
+                    editor.apply();
+
                     Intent i = new Intent(SingleAdvertFragment.this.getActivity(), MainActivity.class);
                     startActivity(i);
                     getActivity().finish();
